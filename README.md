5631128d3e1806d6a89c4fbad61811819522755d
Joel Bartelheimer <joel.bartelheimer@gmx.de>
2020-03-29 18:33:45 +0200

    add the differentiate part of lazy-seq's
    add a first description in the README
    
    Signed-off-by: Joel Bartelheimer <joel.bartelheimer@gmx.de>

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# wfpm-clj-code
-Code examples from 'Why Functional Programming Matters' in clojure    and some thought, notes and links to other references
+# 'Why Functional Programming Matters' in clojure
+
+['Why Functional Programming Matters' by John Hughes](https://www.google.com)
+
+After a quick time reading the paper, i opened up a clojure REPL and tried out the things described in the paper.
+The dynamic of the REPL makes it really easy to play around and try out a lot of things
+--- What happens if i change this, could this also work? --- 
+This helped me a lot to understand the described concepts and ideas.
+
--- src/wfpm_clj_code/glue/lazy_evaluation/lazy_seq.clj
+++ src/wfpm_clj_code/glue/lazy_evaluation/lazy_seq.clj
@@ -1,5 +1,5 @@
 (ns wfpm-clj-code.glue.lazy-evaluation.lazy-seq
-  (:require [clojure.math.numeric-tower :refer [abs]]))
+  (:require [clojure.math.numeric-tower :refer [abs expt round]]))
 
 ;; next nx = (x + n/x)/2
 (defn next-sqrt [n x]
@@ -49,6 +49,7 @@
 ;; b (second) is not destructured here, because we had to add it back to the seq.
 ;; cons a value to a lazy-seq is possible and clojure doesn't throw any errors when implementing
 ;; it with destructuring b (second), but the REPL is crashing
+;; improvement: might be possible to destructure with keeping the original to just avoid the 'cons'
 (defn within [eps [a & rest]]
   (if (<= (abs (- a (first rest))) eps)
     (first rest)
@@ -71,6 +72,60 @@
   (relative eps (iterate (partial next-sqrt n) a0)))
 
 (relative-sqrt 1 0.01 9)
+
+;; problem: find a right approximation for h. idea: reuse 'within eps'
+(defn easydiff [f x h]
+  (/ (- (f (+ x h)) (f x)) h))
+
+(def halve #(/ % 2))
+
+(defn differentiate [h0 f x]
+  (map #(easydiff f x %) (iterate halve h0)))
+
+;; e.g: f(x) = y = 3x² + 5
+;; now differentiate a x=1 with h0=2
+(let [fx #(+ (* 3 % %) 5)
+      eps 0.01
+      h0 2
+      x 1]
+  (within eps (differentiate h0 fx x)))
+
+;; optimize the approximation function
+;; returns a lazy-seq
+(defn elimerror [n [a & rest]]
+  (lazy-seq (cons (/ (- (* (first rest) (expt 2 n)) a) (- (expt 2 n) 1)) (elimerror n rest))))
+
+;; clojure? do i really have to define something like log2 by myself? and do i really have to use java.lang.Math?
+(defn log2 [n]
+  (/ (Math/log n) (Math/log 2)))
+
+;; round(log2 ((a−c)/(b−c)−1))
+;; returns a single val
+(defn order [[a b c & rest]]
+  (do
+    (println "a: " a  " b:" b " c:"  c " a-c: " (- a c) " b-c: " (- b c))
+    (round (log2 (-(/ (- a c) (- b c)) 1)))))
+
+;; returns a lazy-seq
+(defn improve [s]
+  (elimerror (order s) s))
+
+(let [fx #(+ (* 3 % %) 5)
+      eps 1
+      h0 100
+      x 1]
+  (within eps (improve (differentiate h0 fx x))))
+
+(let [fx #(+ (* 3 % %) 5)
+      eps 0.001
+      h0 5000
+      x 1.0054]
+  (within eps (improve (improve (differentiate h0 fx x)))))
+;; => causes an ArithmeticException, Divide by zero in 'order'
+;;  the results of the first improve are already so good (the right result) that there is not much to optimize
+;;  actually the first element in the lazy-seq is already the result
+;;  where the error isn't big enough for a 2nd improvements, maybe different start arguments could help
+
 
 ;; takeaway: it was possible to divide the newton-raphson square root algorithm in smaller peaces.
 ;;           These peaces/modules can not only changed independently, they're also synchronised due to the laziness.
